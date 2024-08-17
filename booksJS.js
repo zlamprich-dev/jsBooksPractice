093aaa85a97dacfa39cbb8436d231707bf5aa304
Zach Lamprich <zlamprich.dev@gmail.com>
2024-08-17 16:35:45 -0600

    Function fixes and addition, add HTML divs

--- booksJS.js
+++ booksJS.js
@@ -1,6 +1,25 @@
 const myLibrary = [];
 
 
+function newBook() {
+    let formLocation = document.getElementById("formDiv")
+    let form = document.createElement("form")
+    formLocation.appendChild(form)
+
+    let titleInput = document.createElement("input")
+    titleInput.setAttribute("type", "text")
+    titleInput.setAttribute("placeholder", "Book Title")
+
+    let authorInput = document.createElement("input")
+    authorInput.setAttribute("type", "text")
+    authorInput.setAttribute("placeholder", "Author Name")
+
+    let publishDateInput = document.createElement("input")
+    publishDateInput.setAttribute("type", "text")
+    publishDateInput.setAttribute("placeholder", "Book Title")
+}
+
+
 function addToLibrary() {
     let x = document.createElement("li")
     let input = document.getElementById("addBookField").value
