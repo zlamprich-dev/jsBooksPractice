a9c12ea58edd516024cb6cf36b32b82c3e06cd5c
Zach Lamprich <zlamprich.dev@gmail.com>
2024-08-19 18:05:20 -0600

    Add form, adjust button functions, add overlay effect and styling adjustments

--- booksJS.js
+++ booksJS.js
@@ -1,6 +1,25 @@
 const myLibrary = [];
+let clickState = false
 
 
+
+function createBook() {
+    let x = document.getElementById("overlay")
+    if (!clickState) {
+
+        x.style.display = "boxed"
+    }
+    x.style.display = "none"
+
+}
+
+function overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
 
 function addToLibrary() {
     let input = document.getElementById("addBookField").value
@@ -13,13 +32,11 @@ function addToLibrary() {
 function libraryLoop() {
     let table = document.getElementById("bookInfo")
 
-
     for (let i = 0; i <= myLibrary.length - 1; i++) {
         let books = myLibrary[i]
         let tableRows = document.createElement("tr")
         table.append(tableRows)
         tableRows.append(books)
-
 
         console.log(myLibrary[i])
     }
